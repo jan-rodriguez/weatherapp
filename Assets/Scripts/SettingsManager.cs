de230898be81fb0db52971c0e049f69744384e57
unknown <jkr241@mit.edu>
2014-12-12 02:47:28 -0500

    Updated UI and added settings panel :

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SettingsManager : MonoBehaviour {
+
+	public GameObject settingsPanel;
+	public GameObject weatherPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowSettingsMenu () {
+		settingsPanel.animation.Play ("SettingScrollIn");
+		weatherPanel.animation.Play ("ScrollOut");
+	}
+
+	public void HideSettingsMenu () {
+		settingsPanel.animation.Play ("SettingScrollOut");
+		weatherPanel.animation.Play ("ScrollIn");
+	}
+}
