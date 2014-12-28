e9ed9fe059ad92e28a7564db0cc99649ec74f735
jkr241 <jkr241@mit.edu>
2014-12-27 20:16:35 -0500

    don't know why it didn't add this

---
+++ Assets/Scripts/ChangeTempUnits.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangeTempUnits : MonoBehaviour {
+
+	private Color orange = new Color ();
+	private Image btnImage;
+
+	public bool isSelected;
+	public bool isFarenheit;
+	public ChangeTempUnits otherBtnScript;
+	public WeatherInfoManager weatherMan;
+
+
+	// Use this for initialization
+	void Start () {
+		//Set orange's values
+		orange.r = 1f;
+		orange.g = 0.478f;
+		orange.b = 0f;
+		orange.a = 1f;
+
+		btnImage = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetNewTempUnits() {
+		//Update button color and change temp in main screen
+		if (!isSelected) {
+			btnImage.color = orange;
+			isSelected = true;
+			otherBtnScript.Deselect();
+			weatherMan.IsFarenheit = isFarenheit;
+			weatherMan.SetWeatherTemp();
+		}
+	}
+
+	public void Deselect(){
+		isSelected = false;
+		btnImage.color = Color.white;
+	}
+}
